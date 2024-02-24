7710019443f05a894f92d2ab4f91dd04c8154124
Splines <dominic-plein@gmx.de>
2024-02-24 15:59:30 +0100

    Indicate that path points to a folder, not a file

--- access-point/setup-server.sh
+++ access-point/setup-server.sh
@@ -1,4 +1,4 @@
-sudo cp ./access-point-server.service /etc/systemd/system
+sudo cp ./access-point-server.service /etc/systemd/system/
 sudo systemctl enable access-point-server
 
 # Start service immediately
